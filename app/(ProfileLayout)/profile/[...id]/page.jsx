f8abea7c7d43fd2efde518f5b3fe357d24b613a5
Bayram Çınar <bayram200212@gmail.com>
2024-09-26 12:45:25 +0300

    Update page.jsx

--- app/(ProfileLayout)/profile/[...id]/page.jsx
+++ app/(ProfileLayout)/profile/[...id]/page.jsx
@@ -1,67 +1,54 @@
-'use client'
+"use client";
 
-import Sidebar from '@/components/sidebar'
-import ProfileContainer from '@/containers/Profile/ProfileContainer'
+import Sidebar from "@/components/sidebar";
+import ProfileContainer from "@/containers/Profile/ProfileContainer";
 
-import { useEffect, useState } from 'react'
-import { postAPI, getAPI } from '@/services/fetchAPI'
-import { useSession } from 'next-auth/react'
+import { useEffect, useState } from "react";
+import { postAPI, getAPI } from "@/services/fetchAPI";
+import { useSession } from "next-auth/react";
 
-import CampaginContainer from '@/components/Campaign'
-import Dashboard from '@/components/commonModules/dashboard'
-import MainPage from '@/components/mainAdvertPage/mainPage'
-import Home from '@/app/messages/page'
-import GeneralComponentFinance from '@/components/financeComponents/generalComponentFinance'
-import SocialAreaForUser from '@/components/socialStatistics/socialAreaForUser'
-import SocialPageComponent from '@/components/socialStatistics/socialPage'
-import MembershipInfo from '@/containers/Home/_components/receiverProfile/MembershipInfo'
-import PasswordUpdate from '@/containers/Home/_components/receiverProfile/PasswordUpdate'
-import Page from '@/app/live/page'
+import CampaginContainer from "@/components/Campaign";
+import Dashboard from "@/components/commonModules/dashboard";
+import MainPage from "@/components/mainAdvertPage/mainPage";
+import Home from "@/app/messages/page";
+import GeneralComponentFinance from "@/components/financeComponents/generalComponentFinance";
+import SocialAreaForUser from "@/components/socialStatistics/socialAreaForUser";
+import SocialPageComponent from "@/components/socialStatistics/socialPage";
+import MembershipInfo from "@/containers/Home/_components/receiverProfile/MembershipInfo";
+import PasswordUpdate from "@/containers/Home/_components/receiverProfile/PasswordUpdate";
+import Page from "@/app/live/page";
 
 export default ({ params }) => {
-  const [data, setData] = useState(null)
-  const [profileForSidebar, setProfileForSidebar] = useState(null)
-  const [profileInfo, setProfileInfo] = useState(null)
-  const [activeTab, setActiveTab] = useState('profile')
-  const { data: sessionInfo } = useSession()
-  const [type, setType] = useState('')
-
-  // async function getProfileData() {
-  //   const response = await postAPI(
-  //     "/auth",
-  //     { email: "example@ofistik.com" },
-  //     "POST"
-  //   );
-  //   setData(response);
-  // }
-
-  // useEffect(() => {
-  //   getProfileData();
-  // }, []);
+  const [data, setData] = useState(null);
+  const [profileForSidebar, setProfileForSidebar] = useState(null);
+  const [profileInfo, setProfileInfo] = useState(null);
+  const [activeTab, setActiveTab] = useState("profile");
+  const { data: sessionInfo } = useSession();
+  const [type, setType] = useState("");
 
   const getProfileInfo = async () => {
     try {
       const [profileResponse, sidebarResponse] = await Promise.all([
         getAPI(`/profile/${params.id}/get-profile-provider`),
         getAPI(`/profile/${sessionInfo.user.id}/get-profile-sidebar`),
-      ])
-      setProfileForSidebar(sidebarResponse.data)
-      setProfileInfo(profileResponse.data)
-      setType(sidebarResponse.message)
+      ]);
+      setProfileForSidebar(sidebarResponse.data);
+      setProfileInfo(profileResponse.data);
+      setType(sidebarResponse.message);
     } catch (error) {
-      console.error('Error fetching profile information:', error)
+      console.error("Error fetching profile information:", error);
     }
-  }
+  };
 
   useEffect(() => {
     if (sessionInfo?.user?.id) {
-      getProfileInfo()
+      getProfileInfo();
     }
-  }, [sessionInfo?.user?.id, params])
+  }, [sessionInfo?.user?.id, params]);
 
   const renderContent = () => {
     switch (activeTab) {
-      case 'profile':
+      case "profile":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <ProfileContainer
@@ -71,70 +58,70 @@ export default ({ params }) => {
               refreshProfile={getProfileInfo}
             />
           </div>
-        )
-      case 'campaign':
+        );
+      case "campaign":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <CampaginContainer />
           </div>
-        )
-      case 'dashboard':
+        );
+      case "dashboard":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <Dashboard />
           </div>
-        )
-      case 'addsense':
+        );
+      case "addsense":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <MainPage />
           </div>
-        )
-      case 'messages':
+        );
+      case "messages":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <Home />
           </div>
-        )
-      case 'finance':
+        );
+      case "finance":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <GeneralComponentFinance />
           </div>
-        )
-      case 'social':
+        );
+      case "social":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <SocialAreaForUser />
           </div>
-        )
-      case 'socialStatistic':
+        );
+      case "socialStatistic":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <SocialPageComponent />
           </div>
-        )
-      case 'create-meeting':
+        );
+      case "create-meeting":
         return (
           <div className="max-h-[100vh] w-full overflow-y-scroll">
             <Page />
           </div>
-        )
-      case 'settings':
+        );
+      case "settings":
         return (
           <div className='bg-grayBg w-full min-h-screen pt-20 sm:pt-36 pb-10 p-3 overflow-auto"'>
             <div className="max-w-4xl mx-auto">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
-                <MembershipInfo profileInfo={profileInfo} type={'PROVIDER'} />
+                <MembershipInfo profileInfo={profileInfo} type={"PROVIDER"} />
                 <PasswordUpdate />
               </div>
             </div>
           </div>
-        )
+        );
       default:
-        return null
+        return null;
     }
-  }
+  };
 
   return (
     <div className="flex ">
@@ -146,5 +133,5 @@ export default ({ params }) => {
       />
       {renderContent()}
     </div>
-  )
-}
+  );
+};
